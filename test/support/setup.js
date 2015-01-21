4061dd1bbdc2ee6ecb706e52682e7ae22384d5e4
Zack Hewison <zack@x-team.com>
2015-01-21 11:39:37 +1100

    Fixing whitespace issues in tests

--- test/support/setup.js
+++ test/support/setup.js
@@ -11,8 +11,8 @@ var ctx = {
     hello: 'Hello, World',
     ten: 10,
     two: 2,
-    arr: [0, 1, 2, 3, 4],
-    rarr: [9, 4, 3, 2, 8],
+    arr: [0,1,2,3,4],
+    rarr: [9,4,3,2,8],
     iarr: [5, 6, 7, 8],
     a: {
         b: {
@@ -25,17 +25,16 @@ var ctx = {
         6: 'six',
         7: 'seven'
     },
-    func1: function () {
-        return 'FUNC1';
-    },
-    func2: function (x) {
-        return x;
-    },
-    harr: [{
-        foo: 'bar'
-    }, {
-        foo2: 'bar2'
-    }]
+    func1: function() { return 'FUNC1'; },
+    func2: function(x) { return x; },
+    harr: [
+        {
+            foo: 'bar'
+        },
+        {
+            foo2: 'bar2'
+        }
+    ]
 };
 
 function esc(str) {
@@ -43,7 +42,7 @@ function esc(str) {
 }
 
 function expect(source, opts, expected) {
-    if (arguments.length === 2) {
+    if(arguments.length === 2) {
         expected = opts;
         opts = {};
     }
@@ -54,7 +53,7 @@ function expect(source, opts, expected) {
     opts.outputPrecompiledSourceToConsole = false;
     opts.outputErrorToConsole = true;
 
-    it('Wash.render("' + esc(source) + '") => "' + esc(expected) + '"', function () {
+    it('Wash.render("' + esc(source) + '") => "' + esc(expected) + '"', function() {
         var wash = new Wash(source, opts);
         var actual = wash.render(ctx);
         assert.strictEqual(actual, expected);
@@ -65,15 +64,15 @@ function expectCompileError(source, opts) {
     opts = opts || {};
     opts.throwOnRuntimeError = true;
 
-    it('Wash.render("' + esc(source) + '") expect compile error [throwOnError=true]', function () {
+    it('Wash.render("' + esc(source) + '") expect compile error [throwOnError=true]', function() {
         opts.throwOnCompileError = true;
-        assert.throws(function () {
+        assert.throws(function() {
             var wash = new Wash(source, opts);
             wash.precompile();
         });
     });
 
-    it('Wash.render("' + esc(source) + '") expect compile error [throwOnError=false]', function () {
+    it('Wash.render("' + esc(source) + '") expect compile error [throwOnError=false]', function() {
         opts.throwOnCompileError = false;
         var wash = new Wash(source, opts);
         var precompiled = wash.precompile();
@@ -83,7 +82,7 @@ function expectCompileError(source, opts) {
 }
 
 function expectRuntimeError(source, opts, expected) {
-    if (arguments.length === 2) {
+    if(arguments.length === 2) {
         expected = opts;
         opts = {};
     }
@@ -91,16 +90,16 @@ function expectRuntimeError(source, opts, expected) {
     opts = opts || {};
     opts.throwOnCompileError = true;
 
-    it('Wash.render("' + esc(source) + '") expect runtime error [throwOnError=true]', function () {
+    it('Wash.render("' + esc(source) + '") expect runtime error [throwOnError=true]', function() {
         opts.throwOnRuntimeError = true;
         var wash = new Wash(source, opts);
         var precompiled = wash.precompile();
-        assert.throws(function () {
+        assert.throws(function() {
             precompiled.render(ctx);
         });
     });
 
-    it('Wash.render("' + esc(source) + '") expect runtime error [throwOnError=false]', function () {
+    it('Wash.render("' + esc(source) + '") expect runtime error [throwOnError=false]', function() {
         opts.throwOnRuntimeError = false;
         var wash = new Wash(source, opts);
         var precompiled = wash.precompile();
